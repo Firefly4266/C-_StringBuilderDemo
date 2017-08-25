a34b79fc360560e6562d152745bfcb54ee7d2a93
Firefly4266 <gaines.johnp@gmail.com>
2017-08-24 20:59:26 -0700

    Building stringbuilder demo.

---
+++ StringBuilder/StringBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+public class StringBuilderDemo
+{
+    public static void Main (string[] args)
+    {
+        StringBuilder build = new StringBuilder();
+        Console.WriteLine("Capacity = {0}", build.Capacity);
+        build.Append("This is the first sentence.\n");
+        Console.WriteLine("Capacity = {0}", build.Capacity);
+        build.Append("This is the second sentence.\n");
+        Console.WriteLine("Capacity = {0}", build.Capacity);
+        build.Append("This is the last sentence.\n");
+        Console.WriteLine("Capacity = {0}", build.Capacity);
+        string str = build.ToString();
+        Console.Write(str);
+    }
+}
